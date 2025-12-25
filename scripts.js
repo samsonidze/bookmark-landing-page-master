e50819b34c9a31aa5dfaa375c62bf4920ec987f8
Giorgi Samsonidze <119052018+samsonidze@users.noreply.github.com>
2025-12-25 15:31:41 +0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,12 +5,39 @@ const easyS = document.getElementById("easyS");
 const optionsF = document.getElementById("optionsF");
 const line = document.querySelector(".line");
 
+const oneClick = document.querySelector(".oneClick");
+const intelligent = document.querySelector(".intelligent");
+const share = document.querySelector(".share");
+
 simpleB.addEventListener("click", () => {
   line.style.left = "0";
+  oneClick.style.opacity = "1";
+  oneClick.style.display = "flex";
+
+  intelligent.style.opacity = "0";
+  intelligent.style.display = "none";
+  share.style.opacity = "0";
+  share.style.display = "none";
 });
 speedyS.addEventListener("click", () => {
   line.style.left = "35%";
+  oneClick.style.opacity = "0";
+  oneClick.style.display = "none";
+
+  intelligent.style.opacity = "1";
+  intelligent.style.display = "flex";
+
+  share.style.opacity = "0";
+  share.style.display = "none";
 });
 easyS.addEventListener("click", () => {
   line.style.left = "70%";
+  oneClick.style.opacity = "0";
+  oneClick.style.display = "none";
+
+  intelligent.style.opacity = "0";
+  intelligent.style.display = "none";
+
+  share.style.opacity = "1";
+  share.style.display = "flex";
 });
